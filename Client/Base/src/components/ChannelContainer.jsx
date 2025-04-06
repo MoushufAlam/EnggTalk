285f41d5fefa2babc77f2d3972e0016faa5758c5
MoushufAlam <rishu78698@gmail.com>
2025-04-06 18:55:06 +0530

    bug

--- Client/Base/src/components/ChannelContainer.jsx
+++ Client/Base/src/components/ChannelContainer.jsx
@@ -21,13 +21,12 @@ function ChannelContainer({isCreating,setIsCreating ,isEditing,createType ,setIs
 
     )
   }
-
-  const EmptyState = ()=> {
+  const EmptyState = ()=> (
     <div className="channel-empty__container">
       <p className="channel-empty__first">This is the beginning of your chat history</p>
       <p className="channel-empty__second">Send messages, attachments, links, emojis, and more!</p>
     </div>
-  }
+  )
 
   return (
     <div className='channel__container'>
