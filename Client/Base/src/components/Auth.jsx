22b609a1fcf8fe34a986e85ec3362ecae56a0be7
MoushufAlam <rishu78698@gmail.com>
2025-04-13 18:50:25 +0530

    changed

--- Client/Base/src/components/Auth.jsx
+++ Client/Base/src/components/Auth.jsx
@@ -27,7 +27,7 @@ function Auth() {
         
         const { username , password , phoneNumber , avatarURL} = form;
 
-        const URL = 'http://localhost:5000/auth'
+        const URL = 'https://engg-talk.vercel.app/auth/auth'
 
         const {data : {token, userId , hashedPassword , fullName}} = await axios.post(`${URL}/${isSignup?'signup':'login'}`,{
             username,password,fullName: form.fullName,phoneNumber,avatarURL
