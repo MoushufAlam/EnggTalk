601740c3d745c427be2c6df5b3747bfddfc511f8
MoushufAlam <rishu78698@gmail.com>
2025-04-13 18:33:44 +0530

    serverless

--- Server/api/index.js
+++ Server/api/index.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 
 const app = express();
 
-const authRoutes = require('./routes/auth')
+const authRoutes = require('../routes/auth')
+const serverless = require('serverless-http')
 
-const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 
@@ -44,4 +44,5 @@ app.post('/',(req,res)=> {
 
 app.use('/auth', authRoutes)
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+module.exports = app
+module.exports.handler = serverless(app)
